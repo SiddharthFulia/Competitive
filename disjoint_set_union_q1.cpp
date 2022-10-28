96759a6abf23387fec71400aae37bfc887870138
Siddharth <siddharthfulia18092002@gmail.com>
2022-10-28 09:30:08 +0530

    Time complexity is known as amortized time complexity O(aplha(n)),inverse Ackermann function is alpha(n) where the time complexity grows very very slowly

--- disjoint_set_union_q1.cpp
+++ disjoint_set_union_q1.cpp
@@ -1,3 +1,4 @@
+//https://www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/city-and-flood-1/
 #include <bits/stdc++.h>
 using namespace std;
 // using namespace std::chrono;
